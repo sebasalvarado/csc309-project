b6fd745275e5dd43824a5889b58cbf3760768c83
radhika2896 <radhika.krishnan@mail.utoronto.ca>
2016-11-22 17:06:38 -0500

    added Post Listing

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import PostNewListing from './PostNewListing';
+import './index.css';
+
+render(
+  <PostNewListing />,
+  document.getElementById('root')
+);
