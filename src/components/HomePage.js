15788377826c28819f88468cbb12e64d6de4d13c
radhika2896 <radhika.krishnan@mail.utoronto.ca>
2016-11-23 18:12:59 -0500

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import ViewItem from './ViewItem';
 
 class HomePage extends Component{
   render(){
     return(
       <div>
-        
+        <ViewItem/>
       </div>
     );
   }
