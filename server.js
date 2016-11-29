c1ddddbdb028fe58eda8369a19bca2aefdf75d3a
Ian <ian.hibbert.a@gmail.com>
2016-11-29 12:17:29 -0500

    initialized app for authentication

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var routes = require('./routes/routes.js');
-var bodyParser = require('body-parser');
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-var app = express();
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var routes = require('./routes/routes');
 
 /* User res.render to load up ejs files */
 // Set the view engines to ejs
@@ -13,6 +21,17 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 /* Get the index page */
 app.get('/', function(req, res) {
