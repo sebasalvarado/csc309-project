7f6f7117a2b7081b24aad556d1feeeda0d55dd9b
Sebastian Alvarado <sebastian.alvarado@mail.utoronto.ca>
2016-11-29 12:38:28 -0500

    Adding pg for managing postgresql

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -2,3 +2,11 @@ import express from 'express';
 import validate from 'express-validation';
 import paramValidation from '../../config/param-validation';
 import listingCtrl from '../controllers/listing.controller';
+
+const router = express.Router();
+
+/** GET list of all listings in the system**/
+router.route('/')
+  .get(listingCtrl.list)
+/** POST Add a new listing **/
+  .post(listingCtrl.create);
