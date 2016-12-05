fd34dc8247da1fc26bee4103a5eb30c27408cb50
Aaron Colter <aaron.colter@mail.utoronto.ca>
2016-12-04 19:56:41 -0500

    almsot complete user page

--- assets/scripts/profile-script.js
+++ assets/scripts/profile-script.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
 
-      $.get('/api/myrequest', function(data) {
-        for (var i = 0; i < data.length; i++) {
-          var row = '<tr><td>' + data[i].item +
-          '</td><td>' + data[i].category +
-          '</td><td>' + data[i].description +
-          '</td><td>' + data[i].date +
-          '</td><td>' + data[i].email +
-          '</td></tr>';
-          $("#userInfo tbody").append(row);
-        }
-      });
+  var username = window.location.pathname.split("/")[1];
+  var path = '/api/user/' + username;
+
+    $.get(path, function(data) {
+      console.log(data);
+        var row = '<tr><td>' + data[0].username +
+        '</td><td>' + data[0].first_name +
+        '</td><td>' + data[0].last_name +
+        '</td><td>' + data[0].email +
+        '</td><td>' + data[0].phonenumber +
+        '</td><td>' + data[0].address+
+        '</td></tr>';
+        $("#userInfo tbody").append(row);
+    });
 });
